e6661f3d544b2f446f1b162a6a14787adc311bff
qmamorup <96193979+qmamorup@users.noreply.github.com>
2022-03-15 19:56:35 +0300

    Add project files.

---
+++ Helpers.h
@@ -0,0 +1,4 @@
+int Sume(int a, int b)
+{
+	return a + b ^ 2;
+}
---
+++ Main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "Helpers.h"
+
+int main()
+{
+	int result = Sume(10, 20);
+
+	std::cout << "Result = " << result << std::endl;
+
+	return 0;
+}
